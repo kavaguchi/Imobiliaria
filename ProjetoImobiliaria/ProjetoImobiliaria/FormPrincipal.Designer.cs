4ef450781d675d3f5ed028a07932dea9f2cd0a4d
lopesguino <lopesguino@gmail.com>
2021-03-03 19:35:28 -0300

    botaoaAG

--- ProjetoImobiliaria/ProjetoImobiliaria/FormPrincipal.Designer.cs
+++ ProjetoImobiliaria/ProjetoImobiliaria/FormPrincipal.Designer.cs
@@ -29,13 +29,33 @@ namespace ProjetoImobiliaria
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Aguinaldo";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "FormPrincipal";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
