b0e77eaa83d73187c6466cab415f358508babf98
lopesguino <lopesguino@gmail.com>
2021-03-03 19:55:21 -0300

    pasta imovel

---
+++ ProjetoImobiliaria/ProjetoImobiliaria/Classes/Imovel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoImobiliaria.Classes
+{
+    class Imovel
+    {
+			public int Codigo { get; set; }
+			public string TipoImovel { get; set; }
+			public string Endereco { get; set; }
+			public string Area { get; set; }
+			public double Metragem { get; set; }
+			public double Valor { get; set; }
+			public int QuantidadeDeQuartos { get; set; }
+			public int QuantidadeDeBanheiros { get; set; }
+			public int Andar { get; set; }
+			public string Observacao { get; set; }
+			public int NumeroDeVagas { get; set; }
+			public double ValorDoCondominio { get; set; }
+			public double IdadeDoImovel { get; set; }
+			public bool Piscina { get; set; }
+			public bool Financia { get; set; }
+			public bool Permuta { get; set; }
+			public List<string> Imagens { get; set; }
+
+
+		
+	}
+}
