40a6ae08d72150deca16563ce2b7015b9de0ffed
lopesguino <lopesguino@gmail.com>
2021-03-17 14:05:12 -0300

    17-03-14:05

--- ProjetoImobiliaria/ProjetoImobiliaria/Program.cs
+++ ProjetoImobiliaria/ProjetoImobiliaria/Program.cs
@@ -17,6 +17,36 @@ namespace ProjetoImobiliaria
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormPrincipal());
+
+            
+/*enum TipoPessoa short 
+{
+	Cliente 		= 1,
+	Corretor 		= 2,
+	Proprietario	= 3 
+}
+
+enum TipoTelefone short 
+{
+	Principal 	= 1,
+	Recado 		= 2,
+	Comercial	= 3,
+	Residencial = 4,
+	Celular		= 5
+}
+
+enum TipoImovel short
+{
+	Terreno 	= 1,
+	Casa		= 2,
+	Apartamento	= 3,
+	Chacara		= 4,
+	Galpao		= 5
+}
+*/
+
+
         }
+
     }
 }
